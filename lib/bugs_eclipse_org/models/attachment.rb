b2a29decafbc75f1270d352940c2f36014ebcaba
Pavol Zbell <pavol.zbell@gmail.com>
2015-11-22 23:13:16 +0100

    Do not use inheritance column for Attachments

--- lib/bugs_eclipse_org/models/attachment.rb
+++ lib/bugs_eclipse_org/models/attachment.rb
@@ -2,4 +2,8 @@ class BugsEclipseOrg::Attachment < ActiveRecord::Base
   belongs_to :bug
   belongs_to :submitter, class_name: :User
   has_many :interactions, dependent: :destroy
+
+  def self.inheritance_column
+    nil
+  end
 end

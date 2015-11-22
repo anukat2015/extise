40e84ea33f6929dc55e694e7e8ec6fb7a474532c
Pavol Zbell <pavol.zbell@gmail.com>
2015-11-22 23:12:52 +0100

    Add some methods to Colored

--- lib/colored_defaults.rb
+++ lib/colored_defaults.rb
@@ -10,6 +10,20 @@ module Colored
   def colorize(s, options = {})
     raw_colorize s, (Colored.colorize_defaults || {}).merge(options)
   end
+
+  alias_method :emphasize, :bold
+
+  def no_foreground
+    self.gsub(/\e\[3[0-7]m/, '')
+  end
+
+  def no_background
+    self.gsub(/\e\[4[0-7]m/, '')
+  end
+
+  def no_extras
+    self.gsub(/\e\[[147]m/, '')
+  end
 end
 
 String.send(:include, Colored)

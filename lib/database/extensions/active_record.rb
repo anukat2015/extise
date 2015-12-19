f2de23d0907bde7e0be9d59dbdc458f70183c200
Pavol Zbell <pavol.zbell@gmail.com>
2015-12-19 17:55:52 +0100

    Add alias_association helper

--- lib/database/extensions/active_record.rb
+++ lib/database/extensions/active_record.rb
@@ -1,4 +1,8 @@
 class ActiveRecord::Base
+  def self.alias_association(new_name, old_name)
+    singleton_class.send :alias_attribute, new_name, old_name
+  end
+
   def self.alias_scope(new_name, old_name)
     singleton_class.send :alias_method, new_name, old_name
   end

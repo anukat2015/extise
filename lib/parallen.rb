4166df065e397eaee5139d14b464f3223434be69
Pavol Zbell <pavol.zbell@gmail.com>
2016-03-01 17:05:43 +0100

    Set Parallen defaults to zero thread workers

--- lib/parallen.rb
+++ lib/parallen.rb
@@ -4,8 +4,9 @@ module Parallen
   extend self
 
   def process(items, options = {}, &block)
+    worker = "in_#{(options[:worker] || :thread).to_s.sub(/[sd]\z/) { |c| c == 's' ? 'ses' : 'ds'}}".to_sym
     options = {
-      "in_#{options[:worker].to_s.sub(/[sd]\z/) { |c| c == 's' ? 'ses' : 'ds'}}".to_sym => options[:count],
+      worker => options[:count] || 0,
       progress: options[:progress],
       start: options[:before_each],
       finish: options[:after_each]

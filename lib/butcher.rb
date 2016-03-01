a084af07f09fe952ed52ce37bbc4aa45731da38a
Pavol Zbell <pavol.zbell@gmail.com>
2016-03-01 16:59:12 +0100

    Add hooks to batch processing

--- lib/butcher.rb
+++ lib/butcher.rb
@@ -2,9 +2,16 @@ module Butcher
   extend self
 
   def process(items, options = {}, &block)
-    batch_size = options[:batch_size] || 1000
-    return items.in_batches of: batch_size, &block if items.respond_to? :in_batches
-    return items.each_slice batch_size, &block if items.respond_to? :each_slice
+    batch_size = options[:batch_size] || 10
+    index, block_with_hooks = 0, -> (batch) do
+      options[:before_batch].call batch, index if options[:before_batch]
+      results = block.call batch
+      options[:after_batch].call batch, index, results if options[:after_batch]
+      index += 1
+      results
+    end
+    return items.in_batches of: batch_size, &block_with_hooks if items.respond_to? :in_batches
+    return items.each_slice batch_size, &block_with_hooks if items.respond_to? :each_slice
     raise ArgumentError
   end
 end

8e119aeb9e0c425ee4ce7778b8fa8a3b05f33830
Pavol Zbell <pavol.zbell@gmail.com>
2016-03-05 23:28:41 +0100

    Refactor and improve main entry point

--- lib/extise/src/sk/stuba/fiit/extise/Main.java
+++ lib/extise/src/sk/stuba/fiit/extise/Main.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import com.google.common.base.Function;
 import com.google.common.base.Joiner;
+import com.google.common.reflect.ClassPath;
+import com.google.common.reflect.ClassPath.ClassInfo;
 
 import org.elasticsearch.common.cli.commons.CommandLine;
 import org.elasticsearch.common.cli.commons.CommandLineParser;
@@ -15,6 +17,8 @@ import org.elasticsearch.common.cli.commons.UnrecognizedOptionException;
 
 import static java.lang.System.err;
 import static java.lang.System.out;
+import static java.lang.reflect.Modifier.isAbstract;
+import static java.lang.reflect.Modifier.isPublic;
 import static java.util.Arrays.asList;
 import static java.util.Arrays.copyOfRange;
 
@@ -27,11 +31,13 @@ public final class Main {
   public static final String VERSION = "0.0.0";
 
   public static void main(final String ... args) throws Exception {
-    bootstrap(options(args));
+    runBootstrap(processOptions(args));
   }
 
+  private static final String[] knownPackages = { "sk.stuba.fiit.extise.dom", "sk.stuba.fiit.extise.map", "sk.stuba.fiit.extise.metric" };
+
   @SuppressWarnings({"unchecked", "rawtypes"})
-  private static void bootstrap(final String ... args) throws Exception {
+  static void runBootstrap(final String ... args) throws Exception {
     Function function = null;
     int index = 0;
 
@@ -41,9 +47,9 @@ public final class Main {
       }
 
       if (function == null) {
-        function = resolve(args[index]);
+        function = resolveFunction(args[index]);
       } else {
-        function = compose(resolve(args[index]), function);
+        function = compose(resolveFunction(args[index]), function);
       }
 
       index ++;
@@ -54,25 +60,32 @@ public final class Main {
     Bootstrap.run(function, paths);
   }
 
-  private static Function<? super Collection<String>, ? extends Collection<?>> resolve(final String name) throws Exception {
-    String[] prefixes = { "sk.stuba.fiit.extise.dom", "sk.stuba.fiit.extise.map", "sk.stuba.fiit.extise.metric" };
+  private static Function<? super Collection<String>, ? extends Collection<?>> resolveFunction(final String name) throws Exception {
+    if (name.contains(".")) {
+      return instantiateFunction(Class.forName(name));
+    }
 
-    for (String prefix: prefixes) {
+    for (String prefix: knownPackages) {
       try {
-        return Function.class.cast(Class.forName(prefix + "." + name).newInstance());
+        return instantiateFunction(Class.forName(prefix + "." + name));
       } catch (ClassNotFoundException e) {
         continue;
       }
     }
 
-    throw new ClassNotFoundException("{" + Joiner.on(",").join(prefixes) + "}." + name);
+    throw new ClassNotFoundException("{" + Joiner.on(",").join(knownPackages) + "}." + name);
   }
 
-  private static String[] options(final String ... args) throws Exception {
+  private static Function<? super Collection<String>, ? extends Collection<?>> instantiateFunction(final Class<?> type) throws Exception {
+    return Function.class.cast(type.newInstance());
+  }
+
+  static String[] processOptions(final String ... args) throws Exception {
     int index = asList(args).indexOf("--");
 
     Options options = new Options();
 
+    options.addOption("list", false, "");
     options.addOption("h", "help", false, "");
     options.addOption("version", false, "");
 
@@ -82,6 +95,7 @@ public final class Main {
 
       if (line.hasOption("help")) {
         out.printf("usage: %s [<options>] <function...> [--] [<file...>]%n%n", PROGRAM);
+        out.printf("        --list%n");
         out.printf("    -h, --help%n");
         out.printf("        --version%n%n");
         System.exit(0);
@@ -92,14 +106,40 @@ public final class Main {
         System.exit(0);
       }
 
+      if (line.hasOption("list")) {
+        ClassLoader loader = Main.class.getClassLoader();
+        ClassPath path = ClassPath.from(loader);
+
+        for (String prefix: knownPackages) {
+          out.printf("%s%n", prefix);
+
+          for (ClassInfo info: path.getTopLevelClasses(prefix)) {
+            String name = info.getSimpleName();
+
+            if (name.equals("package-info")) {
+              continue;
+            }
+
+            Class<?> type = Class.forName(info.getName(), false, loader);
+            int modifiers = type.getModifiers();
+
+            if (Function.class.isAssignableFrom(type) && !isAbstract(modifiers) && isPublic(modifiers)) {
+              out.printf("  %s%n", name);
+            }
+          }
+        }
+
+        System.exit(0);
+      }
+
       List<String> result = newArrayList(line.getArgs());
 
       if (index == 0 || result.size() == 0) {
-        throw failure("missing argument: <function>", new RuntimeException());
+        throw reportFailure("missing argument: <function>", new RuntimeException());
       }
 
       if (index + 1 == args.length) {
-        throw failure("missing argument: <file>", new RuntimeException());
+        throw reportFailure("missing argument: <file>", new RuntimeException());
       }
 
       if (index > 0) {
@@ -108,13 +148,13 @@ public final class Main {
 
       return result.toArray(new String[result.size()]);
     } catch (UnrecognizedOptionException e) {
-      throw failure("invalid option: " + e.getOption(), e);
+      throw reportFailure("invalid option: " + e.getOption(), e);
     } catch (ParseException e) {
-      throw failure("unable to parse options", e);
+      throw reportFailure("unable to parse options", e);
     }
   }
 
-  private static Exception failure(final String message, final Exception failure) throws Exception {
+  private static Exception reportFailure(final String message, final Exception failure) throws Exception {
     err.printf("%s: %s%n", PROGRAM, message);
     System.exit(1);
     return failure;

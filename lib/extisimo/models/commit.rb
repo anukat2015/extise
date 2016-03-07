5bcbdca323cd5a46bf3ae2eec0e483ee00327a6d
Pavol Zbell <pavol.zbell@gmail.com>
2016-03-07 17:55:03 +0100

    Update models

--- lib/extisimo/models/commit.rb
+++ lib/extisimo/models/commit.rb
@@ -2,7 +2,6 @@ class Extisimo::Commit < ActiveRecord::Base
   include Extisimo::URL::Commit
 
   belongs_to :repository
-  belongs_to :session, foreign_key: :revision_commit_id
   belongs_to :author, class_name: :User
 
   has_many :elements, dependent: :destroy
--- lib/extisimo/models/session.rb
+++ lib/extisimo/models/session.rb
@@ -3,12 +3,9 @@ class Extisimo::Session < ActiveRecord::Base
 
   belongs_to :user
 
-  has_one :original_commit, class_name: :Commit
-  has_one :revision_commit, class_name: :Commit
+  has_one :commit
 
-  alias_association :commit, :revision_commit
-
-  delegate :elements, to: :revision_commit
+  delegate :elements, to: :commit
 
   has_many :interactions, dependent: :restrict_with_exception
   has_many :attachments, through: :interactions

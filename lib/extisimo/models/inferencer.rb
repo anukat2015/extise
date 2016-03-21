276190ad2d0fb7d8d20276f0334c2afcc5ab5f2c
Pavol Zbell <pavol.zbell@gmail.com>
2016-03-21 23:56:17 +0100

    List inferencer and metric targets

--- lib/extisimo/models/inferencer.rb
+++ lib/extisimo/models/inferencer.rb
@@ -1,3 +1,5 @@
 class Extisimo::Inferencer < ActiveRecord::Base
+  TARGETS = %w(element task).freeze
+
   has_many :conceptualities, dependent: :restrict_with_exception
 end
--- lib/extisimo/models/metric.rb
+++ lib/extisimo/models/metric.rb
@@ -1,3 +1,5 @@
 class Extisimo::Metric < ActiveRecord::Base
+  TARGETS = %w(concept element session).freeze
+
   has_many :expertises, dependent: :restrict_with_exception
 end

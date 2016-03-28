ea093888ae8fd0a7e68561c88070ee6104b59e2c
Pavol Zbell <pavol.zbell@gmail.com>
2016-03-28 23:21:46 +0200

    Refactor auto color module

--- lib/auto_color.rb
+++ lib/auto_color.rb
@@ -2,7 +2,7 @@ require 'colored'
 
 module AutoColor
   def self.enable(options = {})
-    target = options[:on].extend self::Output
+    target = options[:on].extend Base
     target.colored = options[:colored] != nil ? !!options[:colored] : true
     target.colorings = options[:colorings].to_h || {}
     target
@@ -12,35 +12,39 @@ module AutoColor
     enable options.merge colored: false
   end
 
-  module Output
+  module Base
+    METHODS = %i(abort inform warn print puts)
+
     attr_accessor :colored
     attr_accessor :colorings
 
-    [:abort, :warn, :print, :puts].each do |method|
-      alias_method "uncolored_#{method}", method
-
-      define_method method do |*args|
-        send "uncolored_#{method}", *enrich(args)
+    def self.extended(target)
+      target.class_eval do
+        METHODS.each do |method|
+          next unless target.respond_to? method, true
+          alias_method "uncolored_#{method}", method
+          define_method method do |*args|
+            send "uncolored_#{method}", *enrich(args)
+          end
+        end
       end
     end
 
     private
 
-    def enrich(args)
-      return colorize(args) if args.is_a? String
-      return args.map { |a| a.is_a?(String) ? colorize(a) : a } if args.is_a? Array
-      args
+    def enrich(a)
+      return colorize(a) if a.is_a? String
+      return a.map { |e| e.is_a?(String) ? colorize(e) : e } if a.is_a? Array
+      a
     end
 
     def colorize(s)
       return s.gsub(/\e\[(\d+)m/, '') unless colored
-
       (colorings || {}).each do |regexp, x|
         next if s !~ regexp
         (s = x.call s, regexp) and next if x.respond_to? :call
         [x].flatten.each { |m| s = s.send m }
       end
-
       s
     end
   end

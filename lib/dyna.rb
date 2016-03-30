1958a232e47ea606427a8948948ebf5526af56c4
Pavol Zbell <pavol.zbell@gmail.com>
2016-03-30 16:09:32 +0200

    Simplify Dyna depencencies

--- lib/dyna.rb
+++ lib/dyna.rb
@@ -1,12 +1,10 @@
-require 'active_support/core_ext/object/blank'
-
 module Dyna
   extend self
 
   def resolve_and_create!(file: nil, library: nil, &block)
     name, directory = File.basename(file, '.rb').underscore, File.dirname(file)
     prefix = File.join '.*', library ? File.dirname(library) : 'lib'
-    modules = directory.sub(/\A#{prefix}/, '').split(File::SEPARATOR).reject(&:blank?)
+    modules = directory.sub(/\A#{prefix}/, '').split(File::SEPARATOR).reject(&:empty?)
     type = (modules << name).map(&:camelize).join('::')
     object = load_and_create!(file: file, type: type, &block).last
     return file, name, type, object

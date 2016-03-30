bb584e1ee67124295a837293c8394ce256c87720
Pavol Zbell <pavol.zbell@gmail.com>
2016-03-30 13:07:55 +0200

    Fix alias_association in ActiveRecord extension

--- lib/database/extensions/active_record.rb
+++ lib/database/extensions/active_record.rb
@@ -1,6 +1,6 @@
 class ActiveRecord::Base
   def self.alias_association(new_name, old_name)
-    singleton_class.send :alias_attribute, new_name, old_name
+    define_method(new_name) { send old_name }
   end
 
   def self.alias_scope(new_name, old_name)

3d630a1f1ca1656a57a65058178af9517bc52214
Pavol Zbell <pavol.zbell@gmail.com>
2016-04-04 20:57:06 +0200

    Fix conflicting method names

--- lib/git_eclipse_org/models/project.rb
+++ lib/git_eclipse_org/models/project.rb
@@ -1,3 +1,3 @@
 class GitEclipseOrg::Project < ActiveRecord::Base
-  has_many :changes, dependent: :destroy
+  has_many :change_list, class_name: :Change, dependent: :destroy
 end
--- lib/git_eclipse_org/models/user.rb
+++ lib/git_eclipse_org/models/user.rb
@@ -1,4 +1,4 @@
 class GitEclipseOrg::User < ActiveRecord::Base
-  has_many :changes, foreign_key: :owner_id, dependent: :destroy
+  has_many :change_list, class_name: :Change, foreign_key: :owner_id, dependent: :destroy
   has_many :reviews, foreign_key: :reviewer_id, dependent: :destroy
 end

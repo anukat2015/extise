67ad16e9ed9432a0583d012513eb66b8857b9b37
Pavol Zbell <pavol.zbell@gmail.com>
2016-04-06 08:40:27 +0200

    Enable delegated reporting

--- lib/extric/reporting.rb
+++ lib/extric/reporting.rb
@@ -1,10 +1,20 @@
 module Extric::Reporting
-  def self.message(metric, user, subject, content = $!)
-    message = "#{metric.class} #{content.respond_to?(:message) ? content.message : (content || '?').to_s}"
+  def self.message(object, user, subject, content = $!)
+    message = "#{object.class} #{content.respond_to?(:message) ? content.message : (content || '?').to_s}"
     message << " on #{subject.class}:#{subject.id} for #{user.class}:#{user.id}"
   end
 
+  attr_accessor :reporting_object
+
   def message(user, subject, content = $!)
-    Extric::Reporting.message self, user, subject, content
+    Extric::Reporting.message reporting_object, user, subject, content
+  end
+
+  def reporting_object=(metric)
+    @reporting_object = metric || raise
+  end
+
+  def reporting_object
+    @reporting_object || self
   end
 end

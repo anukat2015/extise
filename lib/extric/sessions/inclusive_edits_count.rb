4973cbb86afbfb4c563eae8e88bdbf709df0532b
Pavol Zbell <pavol.zbell@gmail.com>
2016-04-06 13:02:38 +0200

    Fix critical bug in inclusive edits count metric

--- lib/extric/sessions/inclusive_edits_count.rb
+++ lib/extric/sessions/inclusive_edits_count.rb
@@ -5,7 +5,9 @@ class Extric::Sessions::InclusiveEditsCount
   include Extric::Sessions
 
   def measure(user, session)
+    # TODO refactor to pure SQL
+
     return unless session_user_matches! user, session
-    { value: session.interactions.where(kind: 'edit').group(:file, :path).count('*').select { |_, c| c > 1 }.sum }
+    { value: session.interactions.where(kind: 'edit').group(:file, :path).count('*').values.select { |c| c > 1 }.sum }
   end
 end

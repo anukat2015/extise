e0a58d5866989978d587f48c83ab79c718038904
Pavol Zbell <pavol.zbell@gmail.com>
2016-04-07 12:18:53 +0200

    Fix some bugs and improve SafeEval

--- lib/safe_eval.rb
+++ lib/safe_eval.rb
@@ -1,31 +1,30 @@
 module SafeEval
   extend self
 
-  def safe_eval(o)
-    Object.new.instance_eval { binding }.eval o
+  def safe_eval(o, context: nil, as: nil)
+    c = context || Object.new
+    o = c.instance_eval { binding }.eval o
+    if as
+      t = [as].flatten
+      unless t.empty? || t.find { |u| o.is_a? u }
+        raise Exception.new "#{__FILE__}:#{__LINE__}: evaluation error, unexpected #{o.class}, expecting #{t * ' or '}"
+      end
+    end
+    o
   end
 
   module IO
     extend self
 
-    def read_and_safe_eval(o, as: nil)
+    def read_and_safe_eval(o, context: nil, as: nil)
       o = o.read if o.is_a? ::IO
-      v = SafeEval.safe_eval(o)
-      t = [as].flatten
-
-      unless t.empty? || t.find { |u| v.is_a? u }
-        raise Exception.new "#{__FILE__}:#{__LINE__}: evaluation error, unexpected #{v.class}, expecting #{as}"
-      end
-
-      v
+      SafeEval.safe_eval o, context: context, as: as
     end
 
-    def read_with_safe_eval(o, eval: nil, as: nil)
+    def read_with_safe_eval(o, eval: nil, context: nil, as: nil)
       raise if eval.nil?
-
       require 'io_open_conditional'
-
-      eval ? read_and_safe_eval(o, as: as) : ::IO.open_or(o).readlines
+      eval ? read_and_safe_eval(o, context: context, as: as) : File.open_or(o).readlines
     end
   end
 end

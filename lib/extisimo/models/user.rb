8088fd0d2c38c3af81dd3ca205833c0de81d8fe8
Pavol Zbell <pavol.zbell@gmail.com>
2016-04-11 22:43:13 +0200

    Add user by name fetching helper

--- lib/extisimo/models/user.rb
+++ lib/extisimo/models/user.rb
@@ -16,7 +16,13 @@ class Extisimo::User < ActiveRecord::Base
 
   alias_scope :has_tasks, :has_assigned_tasks
 
-  def names
-    [name]
+  def self.fetch(n = nil)
+    return Extisimo::User unless n
+    condition = <<-SQL.gsub(/\n/, ' ').squeeze(' ')
+      extisimo_users.name = ? OR
+      bugs_eclipse_org_users.login_name = ? OR bugs_eclipse_org_users.realnames @> ? OR
+      git_eclipse_org_users.username = ? OR git_eclipse_org_users.name = ?
+    SQL
+    Extisimo::User.joins(:bugs_eclipse_org_user, :git_eclipse_org_user).where condition, *[n] * 5
   end
 end

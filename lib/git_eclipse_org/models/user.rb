f17e004f6584df52bdc427a9ddd231c74a6fa060
Pavol Zbell <pavol.zbell@gmail.com>
2016-04-19 02:49:27 +0200

    Remove original user fetchers since they behave ambiguously

--- lib/git_eclipse_org/models/user.rb
+++ lib/git_eclipse_org/models/user.rb
@@ -2,9 +2,4 @@ class GitEclipseOrg::User < ActiveRecord::Base
   has_many :change_list, class_name: :Change, foreign_key: :owner_id, dependent: :destroy
   has_many :messages, foreign_key: :author_id, dependent: :destroy
   has_many :labels, dependent: :destroy
-
-  def self.fetch(*n)
-    return GitEclipseOrg::User if n.blank?
-    GitEclipseOrg::User.where 'ARRAY[username,name] && ?', "{#{n * ','}}"
-  end
 end

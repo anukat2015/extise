f3041c276bd2da568de1d34e6fbf73e1f2e62fb7
Pavol Zbell <pavol.zbell@gmail.com>
2016-04-21 10:09:21 +0200

    Fix metrics caching concern, use cache per method

--- lib/extric/caching.rb
+++ lib/extric/caching.rb
@@ -1,15 +1,14 @@
 module Extric::Caching
   extend ActiveSupport::Concern
 
-  def cache
-    @cache ||= ActiveSupport::Cache.lookup_store :memory_store, size: 16.megabytes
-  end
-
   class_methods do
     def cache_method(name, matcher, options = {}, &block)
-      uncached_name = "uncached_#{name}".to_sym
-      alias_method uncached_name, name
-      define_method name do |*args|
+      size = options.delete(:size) || 16.megabytes
+      cached_name, uncached_name, cache_name = name.to_sym, "uncached_#{name}".to_sym, "@#{name}_cache".to_sym
+      alias_method uncached_name, cached_name
+      define_method cached_name do |*args|
+        cache = instance_variable_get cache_name
+        cache ||= instance_variable_set cache_name, ActiveSupport::Cache.lookup_store(:memory_store, size: size)
         key = matcher.try(:call, *args) || matcher
         cache.fetch(key, options) { send uncached_name, *args, &block }
       end

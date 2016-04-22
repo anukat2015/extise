3eb375a2c0a0a6c8908b19b90a0a6df2443f4b59
Pavol Zbell <pavol.zbell@gmail.com>
2016-04-22 12:05:15 +0200

    Fix open conditional method declarations

--- lib/core_ext/io/open_conditional.rb
+++ lib/core_ext/io/open_conditional.rb
@@ -1,9 +1,9 @@
 class IO
-  def open_if(*args)
+  def self.open_if(*args)
     yield(*args) ? open(*args) : args.first
   end
 
-  def open_or(*args)
+  def self.open_or(*args)
     open_if(*args) { !args.first.is_a? IO }
   end
 end

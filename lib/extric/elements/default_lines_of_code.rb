e8ed1c742bfaa6a3a7f83216bd289ab578b8fe74
Pavol Zbell <pavol.zbell@gmail.com>
2016-04-23 19:54:11 +0200

    Implement default lines of code metrics for elements and sessions

--- lib/extric/elements/default_lines_of_code.rb
+++ lib/extric/elements/default_lines_of_code.rb
@@ -1,9 +1,32 @@
-# NOTE: TODO
+# NOTE: counts lines of code of an element authored by a user before a session
 
 class Extric::Elements::DefaultLinesOfCode
   include Extric::Common
 
   def measure(user, element)
-    # TODO implement
+    session = element.commit.session
+
+    unless session
+      warn message user, element, "session bound by revision #{element.commit.identifier} not found"
+      return
+    end
+
+    commit = session.previous_commit
+    repository = commit.repository
+
+    begin
+      b = blame_recent repository: repository, commit: commit, element: element
+    rescue Rugged::TreeError
+      warn message user, element, "#{element.file} not found at #{commit.identifier} (#{$!})"
+      return
+    end
+
+    c = count_recent blame: b, names: user.eclipse_org_user_names
+    t = b.count
+
+    {
+      blame: { final: c, total: t },
+      value: c
+    }
   end
 end
--- lib/extric/sessions/default_lines_of_code.rb
+++ lib/extric/sessions/default_lines_of_code.rb
@@ -1,9 +1,36 @@
-# NOTE: TODO
+# NOTE: counts lines of code of all elements authored by a user before a session
 
 class Extric::Sessions::DefaultLinesOfCode
   include Extric::Common
 
   def measure(user, session)
-    # TODO implement
+    commit = session.previous_commit
+    repository = commit.repository
+
+    g = open_repository name: repository.name
+    i = commit.identifier
+    n = user.eclipse_org_user_names
+
+    c, t = 0, nil
+
+    commit.elements.each do |element|
+      begin
+        f = element.file
+        b = blame_recent git: g, identifier: i, file: f
+      rescue Rugged::TreeError
+        warn message user, session, "#{element.file} not found at #{commit.identifier} (#{$!})"
+        next
+      end
+
+      c += count_recent blame: b, names: n
+      t = (t || 0) + b.count
+    end
+
+    return unless t
+
+    {
+      blame: { final: c, total: t },
+      value: c
+    }
   end
 end

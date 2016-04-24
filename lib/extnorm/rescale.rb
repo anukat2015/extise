e7b30af6d46280d8004b7e1176e8e64d0de67458
Pavol Zbell <pavol.zbell@gmail.com>
2016-04-25 01:56:36 +0200

    Generalize rescaling normalization

--- lib/extnorm/rescale.rb
+++ lib/extnorm/rescale.rb
@@ -1,9 +1,13 @@
 class Extnorm::Rescale
+  RANGE = 0..1
+
   def initialize(v)
     @min, @max = v.min, v.max
   end
 
   def normalize(x)
-    { min: @min, max: @max, value: (x.to_f - @min) / (@max - @min) }
+    v = (x.to_f - @min) / (@max - @min) * (RANGE.end - RANGE.begin) + RANGE.begin
+
+    { range: RANGE, min: @min, max: @max, value: v }
   end
 end

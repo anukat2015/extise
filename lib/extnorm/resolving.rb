471dcb3098bb72d529f5b450ca180ae69b855205
Pavol Zbell <pavol.zbell@gmail.com>
2016-04-24 15:59:31 +0200

    Fix normalization resolving

--- lib/extnorm/resolving.rb
+++ lib/extnorm/resolving.rb
@@ -2,10 +2,10 @@ require 'dyna'
 
 module Extnorm::Resolving
   def resolve_normalization!(target: nil, name: nil, method: nil)
-    name = "#{name.underscore}:#{method.underscore}"
-    file = File.expand_path "#{method.underscore}.rb", __dir__
-    type = "Extnorm::#{method.camelize}"
-    _, handle = Dyna.load_and_create! file: file, type: type { |c| c.new Daru::Vector[0] }
+    name = "#{name.to_s.underscore}:#{method.to_s.underscore}"
+    file = File.expand_path "#{method.to_s.underscore}.rb", __dir__
+    type = "Extnorm::#{method.to_s.camelize}"
+    _, handle = Dyna.load_and_create!(file: file, type: type) { |c| c.new Daru::Vector[0] }
     raise "invalid handle at #{file}" unless handle.respond_to? :normalize
     c = handle.class
     return target, name, file, type, -> (v) { c.new v }

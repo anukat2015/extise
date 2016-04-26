edbd0a018ea5f5a6b1fb0a82431b4a90c110baa7
Pavol Zbell <pavol.zbell@gmail.com>
2016-04-26 02:40:51 +0200

    Add more associations and scopes to user model

--- lib/extisimo/models/user.rb
+++ lib/extisimo/models/user.rb
@@ -2,19 +2,25 @@ class Extisimo::User < ActiveRecord::Base
   include Extisimo::Reference::User
   include Extisimo::URL::User
 
-  has_many :tasks, foreign_key: :assignee_id, dependent: :destroy
+  has_many :reported_tasks, class_name: :Task, foreign_key: :reporter_id, dependent: :destroy
+  has_many :assigned_tasks, class_name: :Task, foreign_key: :assignee_id, dependent: :destroy
   has_many :posts, foreign_key: :author_id, dependent: :destroy
   has_many :attachments, foreign_key: :author_id, dependent: :destroy
   has_many :commits, foreign_key: :author_id, dependent: :destroy
   has_many :sessions, dependent: :destroy
   has_many :expertises, dependent: :destroy
 
-  scope :has_assigned_tasks, -> { joins(:tasks).distinct.merge Extisimo::Task.assigned_to self }
+  alias_association :tasks, :assigned_tasks
+
   scope :has_reported_tasks, -> { joins(:tasks).distinct.merge Extisimo::Task.reported_by self }
+  scope :has_assigned_tasks, -> { joins(:tasks).distinct.merge Extisimo::Task.assigned_to self }
   scope :has_posts, -> { joins(:posts).distinct }
   scope :has_attachments, -> { joins(:attachments).distinct }
 
   alias_scope :has_tasks, :has_assigned_tasks
+
+  scope :ambiguous, -> { where 'name LIKE ?', "%#{Extisimo::Naming::UNKNOWN_NAME}%" }
+  scope :unambiguous, -> { where 'name NOT LIKE ?', "%#{Extisimo::Naming::UNKNOWN_NAME}%" }
 
   def self.fetch(*n)
     names = n.compact

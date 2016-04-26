36c973b1aa090331e36025ea6ea90da3ffdbb507
Pavol Zbell <pavol.zbell@gmail.com>
2016-04-26 07:41:09 +0200

    Fix time option parsing

--- lib/optbind_handlers.rb
+++ lib/optbind_handlers.rb
@@ -6,12 +6,21 @@ require 'optparse/time'
 require 'optparse/uri'
 
 module OptionBinder::Handlers
-  def matches(argument, regexp)
-    -> (v) { regexp =~ v.to_s ? v : raise(OptionParser::InvalidArgument.new to_arg(argument)) }
+  def matches(regexp)
+    -> (v) { regexp =~ v.to_s ? v : raise(OptionParser::InvalidArgument, v) }
   end
 
   def included_in(*values)
-    -> (v) { values.flatten.map(&:to_s).include?(v.to_s) ? v : raise(OptionParser::InvalidArgument.new to_arg(values)) }
+    -> (v) { values.flatten.map(&:to_s).include?(v.to_s) ? v : raise(OptionParser::InvalidArgument, v) }
+  end
+
+  def as_time_with(special = {})
+    lambda do |v|
+      return unless v
+      s = special[v.to_s.to_sym]
+      return s.arity == 1 ? s.call(v) : s.call if s
+      Time.httpdate(v) rescue Time.parse(v)
+    end
   end
 
   def to_arg(v)

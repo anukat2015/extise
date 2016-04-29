9a77f67c493611ba250505c0c2d5800543660751
Pavol Zbell <pavol.zbell@gmail.com>
2016-04-30 01:12:30 +0200

    Support calls to Extise server by default

--- lib/extise.rb
+++ lib/extise.rb
@@ -11,29 +11,62 @@ module Extise
   self.max_heap_size = '512m'
   self.max_perm_size = '64m'
 
-  def self.open(function: nil, input: STDIN)
-    require 'open3'
+  extend self
 
+  attr_accessor :open_handle
+
+  def resolve(function: nil, input: STDIN)
     functions, files = [function].flatten.compact, []
     files = ['--'] + input.flatten.compact if input.is_a? Array
     input = StringIO.new input if input.is_a? String
-
-    Open3.popen3(command functions + files) do |i, o, e, s|
-      i.tap { input.each_line { |l| i.write l } if files.empty? }.close
-      yield(o, e, s).tap { [o, e].each &:close }
-    end
+    return functions, files, input
   end
 
-  def self.stream(function: nil, input: STDIN)
+  def open(function: nil, input: STDIN)
+    open_handle.open(function: function, input: input) { |*a| yield *a }
+  end
+
+  def stream(function: nil, input: STDIN)
     open function: function, input: input do |o, _, s|
       t = Thread.new { yield o }
       s.value.to_i == 0 ? t.value : false
     end
   end
 
-  def self.read(function: nil, input: STDIN)
+  def read(function: nil, input: STDIN)
     stream function: function, input: input do |o|
       block_given? ? o.each_line { |l| yield l } : o.read
+    end
+  end
+
+  module Command
+    def self.open(function: nil, input: nil)
+      require 'open3'
+      functions, files, input = Extise.resolve function: function, input: input
+      Open3.popen3(Extise.command functions + files) do |i, o, e, s|
+        i.tap { input.each_line { |l| i.write l } if files.empty? }.close
+        yield(o, e, s).tap { [o, e].each &:close }
+      end
+    end
+  end
+
+  class Client
+    attr_accessor :host, :port
+
+    def initialize(host: 'localhost', port: 7153)
+      @host, @port = host, port
+    end
+
+    def open(function: nil, input: nil)
+      functions, files, input = Extise.resolve function: function, input: input
+      options = %w(-s) + (functions + files).map { |a| "-H 'Argument: #{a}'" }
+      options += ["-X POST -H 'Content-type: text/plain; charset=utf-8'", '--data-binary @-'] if input
+      Open3.popen3("curl #{options * ' '} #{host}:#{port}") do |i, o, e, s|
+        i.tap { input.each_line { |l| i.write l } if files.empty? }.close
+        r = o.read.tap { o.close }
+        return false if r =~ /\Aextise server: unknown failure/
+        yield(StringIO.new(r), e, s).tap { e.close }
+      end
     end
   end
 

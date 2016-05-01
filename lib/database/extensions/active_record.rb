52c39c5d7626533893c878f03143aa05de863190
Pavol Zbell <pavol.zbell@gmail.com>
2016-05-01 18:21:31 +0200

    Remove fetch helper from expertise model
    Too much magic happening there, hard to debug

--- lib/database/extensions/active_record.rb
+++ lib/database/extensions/active_record.rb
@@ -10,11 +10,12 @@ class ActiveRecord::Base
   def self.findable?(object, options = {})
     return true if object.is_a? ActiveRecord::Base
     primary_key_class = options.fetch :primary_key_class, Integer
-    return true if primary_key_class && object.is_a?(primary_key_class)
+    polymorphic = options.fetch :polymorphic, false
+    return true if primary_key_class && !polymorphic && object.is_a?(primary_key_class)
     return false unless options.fetch :accept_relations, true
     return true if object.is_a? ActiveRecord::Relation
     return false unless object.is_a? Enumerable
-    object.each { |item| return false if !item.is_a?(ActiveRecord::Base) && (primary_key_class ? !item.is_a?(primary_key_class) : true) }
+    object.each { |item| return false if !item.is_a?(ActiveRecord::Base) && (primary_key_class && !polymorphic ? !item.is_a?(primary_key_class) : true) }
     return true
   end
 end
--- lib/extisimo/models/expertise.rb
+++ lib/extisimo/models/expertise.rb
@@ -10,38 +10,6 @@ class Extisimo::Expertise < ActiveRecord::Base
   end
 
   scope :by, -> (metric) { where metric: metric }
-  scope :on, -> (subject) { where subject.respond_to?(:id) ? { subject: subject } : { subject_type: subject.to_s }}
+  scope :on, -> (subject) { where findable?(subject, polymorphic: true) ? { subject: subject } : { subject_type: subject.to_s }}
   scope :of, -> (user) { where user: user }
-
-  def self.fetch(by: nil, on: nil, of: nil)
-    metric, subject, user = *[by, on, of].map { |o| o.respond_to?(:id) ? o : nil }
-
-    metric = Extisimo::Metric.find by if by.is_a? Integer
-    user = Extisimo::User.find of if of.is_a? Integer
-
-    if metric && !subject && on.is_a?(Integer)
-      index = Extisimo::Metric::TARGETS.index metric.target
-      subject = SUBJECT_TYPES[index].constantize.find on
-    end
-
-    if !subject && on
-      index = Extisimo::Metric::TARGETS.index on.to_s
-      subject = (SUBJECT_TYPES[index] if index) || on.to_s
-    end
-
-    if !metric && by && on
-      type = (subject.respond_to?(:id) ? subject.try(:class).to_s : subject) || on.to_s
-      target = Extisimo::Metric::TARGETS[SUBJECT_TYPES.index type]
-      metric = Extisimo::Metric.find_by! target: target, name: by.to_s
-    end
-
-    subject = SUBJECT_TYPES[Extisimo::Metric::TARGETS.index metric.target] if !subject && metric
-    user = Extisimo::User.find_by! name: of.to_s if !user && of
-
-    relation = Extisimo::Expertise
-    relation = relation.by(metric) if metric
-    relation = relation.on(subject) if subject
-    relation = relation.of(user) if user
-    relation
-  end
 end

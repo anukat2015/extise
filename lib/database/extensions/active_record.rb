da73c911f3bbed3645612c20bc168b84168d3f34
Pavol Zbell <pavol.zbell@gmail.com>
2016-05-01 14:11:49 +0200

    Add findable? helper

--- lib/database/extensions/active_record.rb
+++ lib/database/extensions/active_record.rb
@@ -6,4 +6,15 @@ class ActiveRecord::Base
   def self.alias_scope(new_name, old_name)
     singleton_class.send :alias_method, new_name, old_name
   end
+
+  def self.findable?(object, options = {})
+    return true if object.is_a? ActiveRecord::Base
+    primary_key_class = options[:primary_key_class] || Integer
+    return true if object.is_a? primary_key_class
+    return false unless options[:accept_enumerables]
+    return true if object.is_a? ActiveRecord::Relation
+    return false unless object.is_a? Enumerable
+    object.each { |item| return false unless item.is_a?(ActiveRecord::Base) && item.is_a?(primary_key_class) }
+    return true
+  end
 end

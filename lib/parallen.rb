48909e624a9c57eb4fb2c825ef96ffcac58f541f
Pavol Zbell <pavol.zbell@gmail.com>
2016-05-02 19:24:45 +0200

    Format some code

--- lib/parallen.rb
+++ lib/parallen.rb
@@ -4,7 +4,7 @@ module Parallen
   extend self
 
   def process(items, options = {}, &block)
-    worker = "in_#{(options[:worker] || :thread).to_s.sub(/[sd]\z/) { |c| c == 's' ? 'ses' : 'ds'}}".to_sym
+    worker = "in_#{(options[:worker] || :thread).to_s.sub(/[sd]\z/) { |c| c == 's' ? 'ses' : 'ds' }}".to_sym
     options = {
       worker => options[:count] || 0,
       progress: options[:progress],

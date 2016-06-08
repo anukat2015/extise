9e12db9c0141100b39456b7259842c96b01d2e29
Pavol Zbell <pavol.zbell@gmail.com>
2016-06-08 11:46:36 +0200

    Fix auto describe for empty arrays

--- lib/optbind_auto_describe.rb
+++ lib/optbind_auto_describe.rb
@@ -4,7 +4,7 @@ module OptionBinder::AutoDescribe
   def option(*opts, &handler)
     if opts.size == 1 && opts[0].is_a?(String) && opts[0] != /\A\s*-/
       default = @reader.call(opts[0].split(/\s+/, 2)[0].to_sym) rescue nil
-      opts[0] << " Default #{[default] * ','}" unless default.nil?
+      opts[0] << " Default #{[default] * ','}" if !default.nil? && (!default.respond_to?(:empty?) || !default.empty?)
     end
     super
   end
